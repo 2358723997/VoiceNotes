449420d9eb7a4f08728c63fdcbd22bb95627d0d0
2358723997 <2358723997@qq.com>
2025-11-28 15:41:15 +0800

    Initial Commit

---
+++ VoiceNotes/VoiceNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNotesApp.swift
+//  VoiceNotes
+//
+//  Created by 王积学 on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct VoiceNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
